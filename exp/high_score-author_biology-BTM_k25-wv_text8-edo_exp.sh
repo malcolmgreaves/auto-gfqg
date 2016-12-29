f0acf568d99e81ebfa26f6aa15a0bc81405cd950
Malcolm Greaves <greaves.malcolm@gmail.com>
2016-12-28 16:45:21 -0800

    bugfix

--- exp/high_score-author_biology-BTM_k25-wv_text8-edo_exp.sh
+++ exp/high_score-author_biology-BTM_k25-wv_text8-edo_exp.sh
@@ -11,7 +11,7 @@ echo "Selecting gap-words and distractors..."
 for i in `seq 1 9`;
 do
 	date
-	/target/pack/bin/vocab-filter-gap-word-and-distractor-selection output/author_biology-BTM_topic_modeling/combined_topic_word_vectors-k25_top20words output/high_score-author_biology-BTM_k25-wv_text8/selected_sentences-author_biology_lemma_nostop-BTM_k25-score_0.4$i data/word_vectors/text8-vector.bin output/author_biology-BTM_topic_modeling/output/voca.txt output/high_score-author_biology-BTM_k25-wv_text8/generated_questions_gaps_distractors-score_0.4$i
+	./target/pack/bin/vocab-filter-gap-word-and-distractor-selection output/author_biology-BTM_topic_modeling/combined_topic_word_vectors-k25_top20words output/high_score-author_biology-BTM_k25-wv_text8/selected_sentences-author_biology_lemma_nostop-BTM_k25-score_0.4$i data/word_vectors/text8-vector.bin output/author_biology-BTM_topic_modeling/output/voca.txt output/high_score-author_biology-BTM_k25-wv_text8/vocab_restrict/generated_questions_gaps_distractors-score_0.4$i
 done
 
 echo "Done"
