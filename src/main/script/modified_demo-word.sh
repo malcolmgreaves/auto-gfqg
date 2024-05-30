b88b3e519d673a9c8d01fe721ef48d8dedb44545
Malcolm Greaves <malcolmgreaves@users.noreply.github.com>
2024-05-30 16:12:40 +0900

    Update modified_demo-word.sh
    - bash from env
    - explicit exit w/ error message on unset DEV
    - safer bash script execution

--- src/main/script/modified_demo-word.sh
+++ src/main/script/modified_demo-word.sh
@@ -1,11 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# DEV is the directory where all of your projects are
-cd $DEV
+if [[ "${DEV}" == "" ]]; then
+  echo "ERROR: Must set DEV to be the directory where all of your projects are!"
+  exit 1
+fi
+
+set -euo pipefail
+
+pushd $DEV
 
 # clone and move into project
 git clone git@github.com:dav/word2vec.git
-cd word2vec
+pushd word2vec
 
 # pasted modified "script/demo-word.sh" script that
 # doesn't do the word distance demonstration and
@@ -23,16 +29,14 @@ VECTOR_DATA=$DATA_DIR/text8-vector.bin
 pushd ${SRC_DIR} && make; popd
 
 if [ ! -e $VECTOR_DATA ]; then
-
   if [ ! -e $TEXT_DATA ]; then
     if [ ! -e $ZIPPED_TEXT_DATA ]; then
-            wget http://mattmahoney.net/dc/text8.zip -O $ZIPPED_TEXT_DATA
-        fi
+        wget http://mattmahoney.net/dc/text8.zip -O $ZIPPED_TEXT_DATA
+    fi
     unzip $ZIPPED_TEXT_DATA
-        mv text8 $TEXT_DATA
+    mv text8 $TEXT_DATA
   fi
   echo -----------------------------------------------------------------------------------------------------
   echo -- Training vectors...
   time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow 0 -size 200 -window 5 -negative 0 -hs 1 -sample 1e-3 -threads 12 -binary 0 # the key is `binary 0` means "save as text"
-
 fi
